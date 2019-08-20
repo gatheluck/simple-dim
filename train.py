4b4af27fcab4caafaf65f77ee21fe5cd65145efa
gatheluck <gatheluck+tech@gmail.com>
2019-08-20 19:33:47 +0900

    add dataset

--- train.py
+++ train.py
@@ -1,14 +1,27 @@
 from options import *
+from data import *
 
-from tqdm import tqdm
+import torch
+import torch.optim as optim
+
+import tqdm
 
 if __name__ == '__main__':
     opt = TrainOptions().parse()
 
+    # dataset
+    train_dataset = get_dataset(opt.dataset, train=True, input_size=opt.input_size, num_samples=opt.num_samples)
+    train_loader = DataLoader(train_dataset, batch_size=opt.batch_size, shuffle=True, num_workers=opt.num_workers, pin_memory=opt.cuda)
+
     # model
-    model = 
+    model = Encoder().to(opt.device)
     
+    # optimizer
+    model_optimizer = optim.Adam(model.parameters(), lr = opt.lr)
+    loss_optimizer  = optim.Adam(dim_loss.parameters(), lr = opt.lr)
+    labels = get_labels(opt.dataset)
+    opt.num_classes = len(labels)
 
-    for ep in tqdm(range(opt.num_epochs)):
-
+    for epoch in tqdm.tqdm(range(opt.num_epochs)):
+    
 
