04de0bb4a991c448324574679d5a9975857da5fd
gatheluck <gatheluck@gmail.com>
2019-08-20 11:02:34 +0900

    add train.py

--- train.py
+++ train.py
@@ -1,1 +1,14 @@
 from options import *
+
+from tqdm import tqdm
+
+if __name__ == '__main__':
+    opt = TrainOptions().parse()
+
+    # model
+    model = 
+    
+
+    for ep in tqdm(range(opt.num_epochs)):
+
+
