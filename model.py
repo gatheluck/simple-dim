4cce13028d1563ee4ed35230a56486ceb69a4618
gatheluck <gatheluck+tech@gmail.com>
2019-08-20 19:32:45 +0900

    add unit test for LocalDiscriminator

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-__all__ == [
+__all__ = [
     'LocalDiscriminator',
 ]
 
@@ -14,12 +14,22 @@ class LocalDiscriminator(nn.Module):
     def __init__(self, input_dim):
         super(LocalDiscriminator, self).__init__()
         self.input_dim = input_dim
-        self.conv1 = nn.Conv1d(self.input_dim, 512, kernel_size=1)
-        self.conv2 = nn.Conv1d(512, 512, kernel_size=1)
-        self.conv3 = nn.Conv1d(512, 1, kernel_size=1)
+        self.conv1 = nn.Conv2d(self.input_dim, 512, kernel_size=1)
+        self.conv2 = nn.Conv2d(512, 512, kernel_size=1)
+        self.conv3 = nn.Conv2d(512, 1, kernel_size=1)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
         return self.conv3(x)
 
+if __name__ == "__main__":
+    
+    print("Test LocalDiscriminator Class")
+    input_dim = 192
+    x = torch.randn(16, input_dim, 32,32)
+    D_local = LocalDiscriminator(input_dim)
+    
+    print("input shape: {}".format(x.shape))
+    out = D_local(x)
+    print("outout shape: {}".format(out.shape))
