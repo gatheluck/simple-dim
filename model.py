7170dbdbf93f5e27d335786d9bfd650fbb6c9580
gatheluck <gatheluck+tech@gmail.com>
2019-08-20 19:21:26 +0900

    add LocalDiscriminator

--- model.py
+++ model.py
@@ -1,4 +1,25 @@
+__all__ == [
+    'LocalDiscriminator',
+]
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
+class LocalDiscriminator(nn.Module):
+    '''
+    Implementation of concat-and-convolve local disctiminator.
+    For detail, please see Table 7 in original paper. 
+    '''
+    def __init__(self, input_dim):
+        super(LocalDiscriminator, self).__init__()
+        self.input_dim = input_dim
+        self.conv1 = nn.Conv1d(self.input_dim, 512, kernel_size=1)
+        self.conv2 = nn.Conv1d(512, 512, kernel_size=1)
+        self.conv3 = nn.Conv1d(512, 1, kernel_size=1)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        return self.conv3(x)
 
